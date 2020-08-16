b782939a33191570d4343e394e811c7b16335217
Rivers Yang <rivers.yang@icloud.com>
2020-08-16 15:06:13 +0800

    Implement Display trait for StringTool and Direction, and format source code.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::fmt;
 use structopt::StructOpt;
 
 #[derive(StructOpt, Debug)]
@@ -10,7 +11,7 @@ enum StringTool {
         source: String,
         /// The delimiter which to be used in splitting
         #[structopt(short = "b", long = "by", default_value = ",")]
-        by: String
+        by: String,
     },
     /// Trim source string by given direction
     #[structopt(name = "trim")]
@@ -18,8 +19,8 @@ enum StringTool {
         /// The source string which to be trimed
         source: String,
         #[structopt(subcommand)]
-        direction: Direction
-    }
+        direction: Direction,
+    },
 }
 
 #[derive(StructOpt, Debug)]
@@ -33,21 +34,44 @@ enum Direction {
     Right,
     /// Trim source string from both side
     #[structopt(name = "both")]
-    Both
+    Both,
+}
+
+impl fmt::Display for StringTool {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            StringTool::Split { source, by } => write!(f, "Split \"{}\" by \"{}\" ...", source, by),
+            StringTool::Trim { source, direction } => {
+                write!(f, "Trim \"{}\" on \"{}\" ...", direction, source)
+            }
+        }
+    }
+}
+
+impl fmt::Display for Direction {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Direction::Both => write!(f, "both"),
+            Direction::Left => write!(f, "left"),
+            Direction::Right => write!(f, "right"),
+        }
+    }
 }
 
 fn main() {
     let options = StringTool::from_args();
-    println!("{:?}", options);
-
+    println!("{}", options);
     match options {
-        StringTool::Split {source, by} => split(source, by),
-        StringTool::Trim {source, direction} => trim(source, direction)
+        StringTool::Split { source, by } => split(source, by),
+        StringTool::Trim { source, direction } => trim(source, direction),
     }
 }
 
 fn split(source: String, by: String) {
-    let v: Vec<String> = source.split(by.as_str()).map(|str| String::from(str)).collect();
+    let v: Vec<String> = source
+        .split(by.as_str())
+        .map(|str| String::from(str))
+        .collect();
     let v_iter = v.iter();
     for elem in v_iter {
         println!("{:?}", elem);
@@ -58,6 +82,6 @@ fn trim(source: String, direction: Direction) {
     match direction {
         Direction::Both => println!("{:?}", source.trim()),
         Direction::Left => println!("{:?}", source.trim_start()),
-        Direction::Right => println!("{:?}", source.trim_end())
+        Direction::Right => println!("{:?}", source.trim_end()),
     }
 }
