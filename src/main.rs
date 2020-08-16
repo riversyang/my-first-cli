96e1482e95ee675a003b8144564f7679e2793104
Rivers Yang <rivers.yang@icloud.com>
2020-08-16 14:23:04 +0800

    Complete version 0.1.0

---
+++ src/main.rs
@@ -0,0 +1,63 @@
+use structopt::StructOpt;
+
+#[derive(StructOpt, Debug)]
+#[structopt(name = "stringtool")]
+enum StringTool {
+    /// Split source string into sub strings by given delimiter
+    #[structopt(name = "split")]
+    Split {
+        /// The source string which to be splitted
+        source: String,
+        /// The delimiter which to be used in splitting
+        #[structopt(short = "b", long = "by", default_value = ",")]
+        by: String
+    },
+    /// Trim source string by given direction
+    #[structopt(name = "trim")]
+    Trim {
+        /// The source string which to be trimed
+        source: String,
+        #[structopt(subcommand)]
+        direction: Direction
+    }
+}
+
+#[derive(StructOpt, Debug)]
+#[structopt(name = "direction")]
+enum Direction {
+    /// Trim source string from left
+    #[structopt(name = "left")]
+    Left,
+    /// Trim source string from right
+    #[structopt(name = "right")]
+    Right,
+    /// Trim source string from both side
+    #[structopt(name = "both")]
+    Both
+}
+
+fn main() {
+    let options = StringTool::from_args();
+    println!("{:?}", options);
+
+    match options {
+        StringTool::Split {source, by} => split(source, by),
+        StringTool::Trim {source, direction} => trim(source, direction)
+    }
+}
+
+fn split(source: String, by: String) {
+    let v: Vec<String> = source.split(by.as_str()).map(|str| String::from(str)).collect();
+    let v_iter = v.iter();
+    for elem in v_iter {
+        println!("{:?}", elem);
+    }
+}
+
+fn trim(source: String, direction: Direction) {
+    match direction {
+        Direction::Both => println!("{:?}", source.trim()),
+        Direction::Left => println!("{:?}", source.trim_start()),
+        Direction::Right => println!("{:?}", source.trim_end())
+    }
+}
